8b25cc962c0b5c2988a439389c8c6ff3e0b1a8c6
jkj2449 <jkj8982@gmail.com>
2020-04-30 23:12:11 +0900

    페이징 작업

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,17 +7,28 @@ import { Row } from "antd"
 function LandingPage() {
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
     useEffect(() => {
         const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        fetchMovies(endpoint);
+        
+    }, [])
 
+    const fetchMovies = (endpoint) => {
         fetch(endpoint)
             .then(respone => respone.json())
             .then(response => {
-                setMovies(response.results)
+                setMovies(Movies.concat(response.results))
                 setMainMovieImage(response.results[0])
+                setCurrentPage(response.page)
             })
-    }, [])
+    }
+
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}/movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`
+        fetchMovies(endpoint);
+    }
 
     return (
         <div style={{ width: '100%', margin: '0' }}>
@@ -50,7 +61,7 @@ function LandingPage() {
             </div>
 
             <div style={{ display: 'flex', justifyContent: 'center'}}>
-                <button> Load More</button>
+                <button onClick={ loadMoreItems }> Load More</button>
             </div>
 
         </div>
