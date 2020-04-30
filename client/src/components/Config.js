33fc0c67c986c82f8251f2b801db8fa14468c866
jkj2449 <jkj8982@gmail.com>
2020-04-30 22:37:46 +0900

    랜딩페이지 메인 이미지 작업

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3';
+
+export const IMAGE_URL = 'https:/image.tmdb.org/t/p';
+
+export const API_KEY = '3ed34cb5e59a4a128bfc484fd1b87577';
